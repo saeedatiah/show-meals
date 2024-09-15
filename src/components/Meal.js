cf98114c754d8eddaa0dd5b7a91c232185fe149c
saeedatiah <saeedatiah17@gmail.com>
2024-09-15 21:29:08 +0300

    try img

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import shawrma from './shawrma.jpeg'
 
-const Meal = ({meal}) => {
+const Meal = ({meal}) => {    
+
   return (
-    <div className='rounded-lg w-24 h-24 m-0 border-2 border-black overflow-hidden relative'>
+    <div className='overflow-hidden '>
+<div className='rounded-lg max-w-32 min-w-11 h-24 w-fit   relative bg-cover faded_img' style={{
+      backgroundImage: `url(${shawrma})`,
+    }}  >
     <h1 className='text-green-300 absolute '>{meal.Name}</h1>
 
-    <img src={shawrma} alt="" class="w-fit h-fit object-cover"/>
+    {/* <img src={shawrma} alt="" class="w-fit h-fit object-cover "/> */}
       
     </div>
+    </div>
+    
   )
 }
 
